e56f51fc0ba5ae3ebdba1292ff4dc6e874e710a5
nina <juan.amado.sicat.cleto@gmail.com>
2025-07-08 22:57:01 +0800

    Oh christmas tree!

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+/*
+ * I am going to discard for this program, the C style
+ * So instead of using NULL, I am going to use nullptr
+ */
+
 struct TREE {
     int value;
     TREE *left;
@@ -7,8 +12,14 @@ struct TREE {
     TREE *trunk;
 };
 
-void addNode(TREE *tree) {
-    TREE *ptr1, *ptr2, *n1, *n2, n3;
+void addRootNode(TREE **pStart, int value) {
+    TREE *root, *ptr1, *ptr2;
+
+}
+
+void addNode(TREE **tree, int d1, int d2, int d3) {
+    TREE *ptr1, *ptr2, *n1, *n2, *n3;
+
 
 }
 
